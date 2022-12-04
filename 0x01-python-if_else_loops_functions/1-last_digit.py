e3dadf4e4cdb2975ff836bd2436341c726876903
smile9565 <bad9565@gmail.com>
2022-12-04 05:28:01 -0800

    i love elif

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 if number % 10 > 5:
-    print(f"{number:d} is greater than 5")
+    print(f"last digit 0f {number:d} and is greater than 5")
 elif number % 10 == 0:
-    print(f"{number:d} is 0")
+    print(f"last digit of {number:d} and is 0")
 elif number % 10 < 6 && number % 10 != 0:
-    print(f"{number:d} is less than 6 and not 0")
+    print(f"last digit of {number:d} and is less than 6 and not 0")
