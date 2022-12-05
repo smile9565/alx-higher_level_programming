324c8d6b850967bdc26a183d134be04932873ba4
smile9565 <bad9565@gmail.com>
2022-12-05 06:22:10 -0800

    task3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-answer = 0
-
-for i in range(1, len(sys.argv)):
-    answer += int(sys.argv[i])
-print("{:d}".format(answer))
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    index = 1
+    res = 0
+    while index <= argv_count:
+        res += int(sys.argv[index])
+        index += 1
+    print("{:d}".format(res))
